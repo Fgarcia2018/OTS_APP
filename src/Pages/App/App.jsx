e4ddc3529c6895a8c365c6bea1d6a3af9195b740
Fernando García <andgarcia_24@hotmail.com>
2023-09-20 18:01:41 -0500

    Se realiza enrutamiento de las paginas. Se instala reac-router-dom y se importa browserRouter y useRouer en App.jsx. Se crea componente AppRoutes y se almacena en un array de objetos las rutas

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,15 +1,27 @@
+import { BrowserRouter, useRoutes } from 'react-router-dom';
 import Home from '../Home';
 import Login from '../Login';
 import RecordMaterial from '../RecordMaterial';
 import NotFound from '../NotFound';
+const AppRoutes=()=>{
+  let routes=useRoutes([
+    {path:'/',element: <Home/>},
+    {path:'/login',element: <Login/>},
+    {path:'/record-material',element: <RecordMaterial/>},
+    {path:'/*',element: <NotFound/>},
+  ])
+  return routes;
+
+}
 function App() {
+  
   return (
     <>
-      <div className='bg-cyan-600'>
-        <Home></Home>
-        <Login></Login>
-        <RecordMaterial></RecordMaterial>
-        <NotFound></NotFound>
+      <div className='bg-cyan-600'>    
+      <BrowserRouter>
+          <AppRoutes/> 
+      </BrowserRouter>  
+            
       </div>
      
     </>
